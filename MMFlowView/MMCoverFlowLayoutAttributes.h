3bb5733f84b43d0d592035ccbc923b0e58eeb41e
Markus Mueller <mmlr@gmx.net>
2014-01-14 13:40:47 +0100

    MMCoverFlowLayoutAttributes properties changed to readonly

--- MMFlowView/MMCoverFlowLayoutAttributes.h
+++ MMFlowView/MMCoverFlowLayoutAttributes.h
@@ -10,12 +10,12 @@
 
 @interface MMCoverFlowLayoutAttributes : NSObject
 
-@property NSUInteger index;
-@property CATransform3D transform;
-@property CGRect bounds;
-@property CGPoint position;
-@property CGPoint anchorPoint;
-@property CGFloat zPosition;
+@property (readonly) NSUInteger index;
+@property (readonly) CATransform3D transform;
+@property (readonly) CGRect bounds;
+@property (readonly) CGPoint position;
+@property (readonly) CGPoint anchorPoint;
+@property (readonly) CGFloat zPosition;
 
 - (id)initWithIndex:(NSUInteger)anIndex position:(CGPoint)aPosition size:(CGSize)aSize anchorPoint:(CGPoint)anAnchorPoint transfrom:(CATransform3D)aTransform zPosition:(CGFloat)aZPosition;
 
