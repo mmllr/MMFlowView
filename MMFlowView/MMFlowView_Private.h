7b314e24c3847ec2275afa88825fe6e170d29618
Markus Mueller <mmlr@gmx.net>
2014-02-12 13:08:07 +0100

    Removed unneeded comments from private MMFlowView header

--- MMFlowView/MMFlowView_Private.h
+++ MMFlowView/MMFlowView_Private.h
@@ -34,99 +34,38 @@
 
 @property (assign,nonatomic) BOOL draggingKnob;
 @property (assign) CGFloat mouseDownInKnob;
-
 @property (readwrite,nonatomic) NSUInteger numberOfItems;
-
-/* number of potentialy visible items, taking in account selection scrolled to left- and rightmost index */
 @property (readonly,nonatomic) NSUInteger maximumNumberOfStackedVisibleItems;
-
-/* image cache which holds key-value pairs of image-uids (key) and CGImageRefs (value) */
 @property (readwrite,strong) id<MMFlowViewImageCache> imageCache;
-
-/* the bound content array if bindings are used */
 @property (weak, nonatomic, readonly) NSArray *contentArray;
-
-/* the bound NSArrayController if bindings are used */
 @property (weak, nonatomic, readonly) NSArrayController *contentArrayController;
-
-/* the keypath for the content array in the bound array-controller */
 @property (weak, nonatomic, readonly) NSString *contentArrayKeyPath;
-
-/* dictionary holding bindings */
 @property (strong,nonatomic,readwrite) NSMutableDictionary *bindingInfo;
-
-/* dictionary holding item layer positions (key is the item-image uid, value is a NSNumber containing its index */
 @property (strong,readwrite) NSMutableArray *layerQueue;
-
-/* array of all oberved items if bindings are used */
 @property (nonatomic,copy) NSArray *observedItems;
-
-/* set of all observed keypaths on the image items, dynamically created */
 @property (weak, nonatomic,readonly) NSSet *observedItemKeyPaths;
-
-/* indicates if bindings are enabled */
 @property (nonatomic,readonly) BOOL bindingsEnabled;
 
-/* set containing all types which are based on urls or filepaths */
 + (NSSet*)pathRepresentationTypes;
-
-/* mapping dict for getting uris for representation types */
 + (NSDictionary*)uniformTypesDictionary;
-
-/* default image and gradient */
 + (CGImageRef)defaultImage;
 + (NSArray*)backgroundGradientColors;
 + (NSArray*)backgroundGradientLocations;
-
-/* invoked if mouse enters the frame of a specific layer */
 - (void)mouseEnteredLayerAtIndex:(NSUInteger)anIndex;
-
-/* invoked if mouse exited the frame of a specific layer */
 - (void)mouseExitedLayerAtIndex:(NSUInteger)anIndex;
-
-/* invoked after changing the selection, adjust scroll knob, visible images etc */
 - (void)updateSelectionInRange:(NSRange)invalidatedRange;
-
-/* performs hittesting and returns the modellayer at the hitpoint */
 - (CALayer*)hitLayerAtPoint:(CGPoint)aPoint;
-
-/* returns the corresponding item, which either needs to support the MMFlowViewItem-protocol (datasource) or ist the entity-type in the bound array-controller */
 - (id)imageItemForIndex:(NSUInteger)anIndex;
-
-/* returns the image uid for an item, see MMFlowViewItem protocol */
 - (NSString*)imageUIDForItem:(id)anItem;
-
-/* returns the representation type for an item, see MMFlowViewItem protocol */
 - (NSString*)imageRepresentationTypeForItem:(id)anItem;
-
-/* returns title for an item, see MMFlowViewItem protocol */
 - (NSString*)imageTitleForItem:(id)anItem;
-
-/* returns the image representation for an item, see MMFlowViewItem protocol */
 - (id)imageRepresentationForItem:(id)anItem;
-
-/* returns title for an specific index */
 - (NSString*)titleAtIndex:(NSUInteger)anIndex;
-
-/* returns an autoreleased system provided default image */
 - (CGImageRef)defaultImageForItem:(id)anItem withSize:(CGSize)imageSize;
-
-/* retuns YES if item at index is a movie */
 - (BOOL)isMovieAtIndex:(NSUInteger)anIndex;
-
-/* returns the UTI for item at index */
 - (NSString*)uniformTypeIdentifierAtIndex:(NSUInteger)anIndex;
-
-/* checks image cache for an image with an specific uid, may return NULL */
-- (CGImageRef)lookupForImageUID:(NSString*)anUID;
-
-/* highlights a layer, invoked in dragging */
 - (void)highlightLayer:(CALayer*)aLayer highlighted:(BOOL)isHighlighted cornerRadius:(CGFloat)cornerRadius highlightingColor:(CGColorRef)highlightingColor;
-
-/* starts observing items in a collection */
 - (void)startObservingCollection:(NSArray*)aCollection atKeyPaths:(NSSet*)keyPaths;
-
-/* stops observing items a collection, removes all observations on sub-keypaths */
 - (void)stopObservingCollection:(NSArray*)aCollection atKeyPaths:(NSSet*)keyPaths;
 
 @end
